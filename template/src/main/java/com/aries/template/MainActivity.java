b76e646d457c2207478588d323ef46c9a065f4f4
chengyiluo <chengyiluo@hengtiansoft.com>
2022-06-08 17:26:26 +0800

    【界面】调整界面属性，添加沉浸，添加常亮

--- template/src/main/java/com/aries/template/MainActivity.java
+++ template/src/main/java/com/aries/template/MainActivity.java
@@ -1,8 +1,12 @@
 package com.aries.template;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.aries.library.fast.entity.FastTabEntity;
 import com.aries.library.fast.manager.LoggerManager;
@@ -77,7 +81,7 @@ public class MainActivity extends FastMainActivity implements ISupportActivity {
         LoggerManager.d("OnTabSelectListener:onTabReselect:" + position);
     }
 
-//    @Override
+    //    @Override
 //    protected void onDestroy() {
 //        super.onDestroy();
 //        LoggerManager.i(TAG, "onDestroy");
@@ -102,6 +106,7 @@ public class MainActivity extends FastMainActivity implements ISupportActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ViewControlSettings();
         mDelegate.onCreate(savedInstanceState);
 
         if (findFragment(HomeFragment.class) == null) {
@@ -260,6 +265,89 @@ public class MainActivity extends FastMainActivity implements ISupportActivity {
      */
     public <T extends ISupportFragment> T findFragment(Class<T> fragmentClass) {
         return SupportHelper.findFragment(getSupportFragmentManager(), fragmentClass);
+    }
+
+    /**
+     * 页面属性配置
+     * - 沉浸式
+     * - 长期点亮
+     */
+    public void ViewControlSettings(){
+        boolean keepScreenOn = true;//长亮屏幕
+        boolean isRequestFocus = true;//焦点
+
+        if (getWindow() == null) return;
+        // 是否保持屏幕点亮
+        // 这个判定会让屏幕一直保持点亮状态
+        if(keepScreenOn){
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
+
+        // 将父类的边框设定强制为0，一般这个父类都是最底层的显示对象
+        getWindow().getDecorView().setPadding(0, 0, 0, 0);
+
+        // FLAG_HARDWARE_ACCELERATED 表示这个window是否启动硬件加速,请求硬件加速但不能保证硬件加速生效。
+        // 如果仅是用来启动硬件加速,可以在代码中控制,使用下面的代码给指定window启动硬件加速:
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
+                WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
+
+        // 全屏的窗口下面 会忽略 SOFT_INPUT_ADJUST_RESIZE  ，不会进行调整
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+
+        // 设置全屏
+        // SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN : Activity全屏显示，但是状态栏不会被覆盖掉，而是正常显示，只是Activity顶端布局会被覆盖住
+        // SYSTEM_UI_FLAG_FULLSCREEN : Activity全屏显示，且状态栏被覆盖掉
+        // SYSTEM_UI_FLAG_VISIBLE : 状态栏和Activity共存，Activity不全屏显示。也就是应用平常的显示画面
+        int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+        getWindow().getDecorView().setSystemUiVisibility(option);
+
+        // 去掉状态栏的阴影
+        if(Build.VERSION.SDK_INT == 19){
+            //解决4.4上状态栏闪烁的问题
+            // 设置状态栏为透明并且为全屏模式。
+            // 然后通过添加一个与 StatusBar 一样大小的 View，将 View 的 backgroud 设置为我们想要的颜色，从而实现沉浸式。
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }else if (Build.VERSION.SDK_INT == 20) {
+            // 由于透明主题不能在4.4以前的版本里面使用，
+            // 所以系统样式跟以前没有区别，也就是看不到任何变化，
+            // 这是一个兼容模式，这个模式可以兼容 20
+            setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, true);
+        }else if (Build.VERSION.SDK_INT >= 21) {
+            // 尝试去掉状态栏
+//            int navigationBarColor = 0xffffff;//显示导航栏颜色
+            int navigationBarColor = Color.TRANSPARENT; //去掉导航栏
+            setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, false);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+            // 如果状态栏背景已经被调整颜色，则不需要再镂空
+            getWindow().setNavigationBarColor(navigationBarColor);
+            //尝试兼容部分手机上的状态栏空白问题
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        }
+
+        // 在show之前必须要关闭焦点，这个代码是很有必要的不能删除
+        if(!isRequestFocus){
+            //不获取焦点
+            int flag = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
+            getWindow().setFlags(flag,flag);
+        }
+
+        getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT);
+    }
+
+    /**
+     * 设置 window的FLAG的通用方法
+     * 工具方法
+     * 直接向window的LayoutParams设置FLAG
+     */
+    public void setWindowFlag(final int bits, boolean on) {
+        WindowManager.LayoutParams winParams = getWindow().getAttributes();
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
+        }
+        getWindow().setAttributes(winParams);
     }
 
 }
